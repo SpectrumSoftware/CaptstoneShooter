8543b0ee1c4d615019aa204394a7682ec4caab07
Kyle Charlton <kyle.charlton@outlook.com>
2016-08-03 22:55:38 -0600

    Added clamping to camera rotation
    -Added clamping to camera rotation so you cant do any backflips in
    place.
    -Made mouse axes more modular by having the option to choose what axis
    is being controlled by the MouseLook script.

--- Assets/Resources/Scripts/Player/FPSControl.cs
+++ Assets/Resources/Scripts/Player/FPSControl.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerControl : MonoBehaviour {
+public class FPSControl : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
